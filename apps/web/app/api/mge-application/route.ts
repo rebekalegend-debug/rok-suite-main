12bccc965b5318b223d4aee73581300af3a1b1ef
rebekalegend-debug <rebekalegend@gmail.com>
2026-03-08 20:17:41 +0200

    Update route.ts

--- apps/web/app/api/mge-application/route.ts
+++ apps/web/app/api/mge-application/route.ts
@@ -12,17 +12,12 @@ export async function POST(req: Request) {
       client_email: process.env.GOOGLE_CLIENT_EMAIL,
       private_key: process.env.GOOGLE_PRIVATE_KEY?.replace(/\\n/g,"\n"),
     },
-    const auth = new google.auth.GoogleAuth({
-  credentials:{
-    client_email: process.env.GOOGLE_CLIENT_EMAIL,
-    private_key: process.env.GOOGLE_PRIVATE_KEY?.replace(/\\n/g,"\n")
-  },
   scopes:[
     "https://www.googleapis.com/auth/drive.file",
     "https://www.googleapis.com/auth/spreadsheets"
   ]
 });
-  });
+ 
 
   const drive = google.drive({ version:"v3", auth });
 
