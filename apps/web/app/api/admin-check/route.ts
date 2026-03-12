e7e13a414ef9af28b01cafccc8d310d71b65ab76
rebekalegend-debug <rebekalegend@gmail.com>
2026-03-12 22:18:02 +0200

    Update route.ts

--- apps/web/app/api/admin-check/route.ts
+++ apps/web/app/api/admin-check/route.ts
@@ -2,7 +2,7 @@ export async function POST(req: Request) {
 
   const { password } = await req.json()
 
-  if(password === process.env.ADMIN_PASSWORD){
+  if(password === process.env.NEXT_PUBLIC_ADMIN_PASSWORD){
     return Response.json({ success:true })
   }
 
