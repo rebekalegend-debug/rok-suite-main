b23a6421bc1a09c98a3a13ebeff8acdb596b33da
rebekalegend-debug <rebekalegend@gmail.com>
2026-03-22 02:51:14 +0200

    Update page.tsx

--- apps/web/app/support/page.tsx
+++ apps/web/app/support/page.tsx
@@ -17,16 +17,15 @@ export default function SupportPage() {
 
         {/* Ko-fi iframe */}
 <div className="flex justify-center items-center min-h-[70vh] px-3">
-
-
-    
-    <div className="rounded-2xl overflow-hidden">
+  <div className="w-full max-w-[420px] overflow-hidden rounded-2xl flex justify-center">
       
-  <iframe
+<iframe
   src="https://ko-fi.com/littlequeen/?hidefeed=true&widget=true&embed=true&preview=true"
-  className="border-0 w-full max-w-[426px] md:max-w-[426px] md:-ml-[13px] md:-mr-[13px]"
+  className="border-0 w-full max-w-[426px] md:w-[440px]"
   style={{
     height: '575px',
+    marginLeft: 'auto',
+    marginRight: 'auto',
   }}
   scrolling="no"
   title="littlequeen"
